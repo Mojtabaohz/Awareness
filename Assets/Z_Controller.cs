ac3f063953235f7377aa78776692f081b91cffdb
MojtabaOHz <moji.ostad@gmail.com>
2020-10-01 14:59:58 +0200

    add the zombie asset
    Zombie asset added

---
+++ Assets/Z_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Z_Controller : MonoBehaviour
+{
+    [SerializeField]
+    private Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
